932675c63133f8848f04eca24235e9fa82aab193
anita0711 <anita.ganani90@gmail.com>
2020-10-11 10:42:43 -0700

    Adding JavaScript assignment

--- src/DynamicArticle/DynamicArticle.jsx
+++ src/DynamicArticle/DynamicArticle.jsx
@@ -5,8 +5,13 @@ const DynamicArticle = (props) => {
   return (
     <article>
       {/* this is an example of how you insert props into a react component */}
-      <div>{props.article.title}</div>
-      <HTMLText text={props.article.text} />
+      <header>
+        <h1>{props.article.title}</h1>
+        <address>by {props.article.author} (<a href={props.article.authorEmail}>{props.article.authorEmail}</a>)</address>
+        <time dateTime={props.article.timeStamp}>{props.article.displayDate}</time>
+      </header>
+      <HTMLText text={props.article.text}
+      />     
     </article>
   );
 };
